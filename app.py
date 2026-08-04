c2289109a179f08cf11f83424197198462ba2605
praveenreddythalla <praveenreddythalla1994@gmail.com>
2026-08-05 00:55:22 +0530

    Added /reset endpoint for clearing votes (Version 2)

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+votes = {}
+voter_list = [
+    "Praveen", "Ravi", "Suresh", "Naresh", "Kiran", "Vamsi",  # unique
+    "Praveen", "Ravi", "Praveen", "Kiran"
+]
+def process_votes():
+    for name in voter_list:
+        votes[name] = votes.get(name, 0) + 1
 
-@app.route("/")
-def home():
+process_votes()
 
-    return "Welcome to the App"
+# @app.route("/")
+# def home():
+
+#     return "Welcome to the App"
 @app.route("/health")
 def health():
     return "App is running"
+
+@app.route("/")
+def home():
+    return "Voting App Running"
+
+@app.route("/results")
+def results():
+    return jsonify(votes)
+
+#version-2
+@app.route("/reset")
+def reset():
+    votes.clear()
+    return jsonify({"message": "All votes have been reset"})
 
 if __name__ == "__main__":
 
